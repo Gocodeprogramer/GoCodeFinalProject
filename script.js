ea73979ba75f890f9cf66016c5b359f82150eaa0
Gocodeprogramer <67921885+Gocodeprogramer@users.noreply.github.com>
2020-08-05 11:18:57 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,74 @@
+const selectionButtons = document.querySelectorAll('[data-selection]')
+const finalColumn = document.querySelector('[data-final-colum]')
+const computerScoreSpan = document.querySelector('[data-computer-score')
+const yourScoreSpan = document.querySelector('[data-your-score')
+const SELECTIONS = [
+    {
+        name: 'rock',
+        emoji:'✊',
+        beats: 'scissors',
+    },
+
+    {
+        name: 'paper',
+        emoji:'✋',
+        beats: 'rock',
+    },
+
+    {
+        name: 'scissors',
+        emoji:'✌',
+        beats: 'paper',
+    }
+]
+    
+    
+
+selectionButtons.forEach(selectionButton => {
+   selectionButton.addEventListener('click', e => {
+      const selectionName = selectionButton.dataset.selection
+      const selection = SELECTIONS.find(selection => selection.name === selectionName)
+      makeSelection(selection)
+  })
+})
+
+
+
+function makeSelection(selection) { 
+    const computerSelection = randomSelection()
+    const yourWinner = isWinner(selection, computerSelection)
+    const computerWinner = isWinner(selection, yourWinner)
+    console.log(computerSelection)
+    
+}
+function incrementScore(scoreSpan) {
+    scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1
+}
+
+
+if (yourWinner)incrementScore(yourScoreSpan)
+if (computerWinner)incrementScore(computerScoreSpan)
+
+
+
+addSelectionResult(computerWinner, computerSelection)
+addSelectionResult(selection, yourWinner)
+
+function addSelectionResult(selection, winner) {
+    const div = document.createElement('div')
+    div.innerText = selection.emoji
+    div.classList.add('result-selection')
+    if (winner) div.classList.add('winner') 
+    finalColumn.after(div)   
+}
+    
+
+
+function isWinner(selection, opponentSelection) {
+    return selection.beats === opponentSelection.name
+}
+
+function randomSelection() {
+    const randomIndex = Math.floor(Math.random() * SELECTIONS.length)
+    return SELECTIONS[randomIndex]
+}
