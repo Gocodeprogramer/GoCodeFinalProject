e4df6ba663e4f44d74dba9bbe1b4c7f7fa467286
ronjacobch <58380756+ronjacobch@users.noreply.github.com>
2020-08-06 11:31:55 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,75 +1,16 @@
-const selectionButtons = document.querySelectorAll('[data-selection]')
-const finalColumn = document.querySelector('[data-final-colum]')
-const computerScoreSpan = document.querySelector('[data-computer-score')
-const yourScoreSpan = document.querySelector('[data-your-score')
-const SELECTIONS = [
-    {
-        name: 'rock',
-        emoji:'✊',
-        beats: 'scissors',
-    },
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
 
-    {
-        name: 'paper',
-        emoji:'✋',
-        beats: 'rock',
-    },
-
-    {
-        name: 'scissors',
-        emoji:'✌',
-        beats: 'paper',
-    }
-]
-    
-    
-
-selectionButtons.forEach(selectionButton => {
-   selectionButton.addEventListener('click', e => {
-      const selectionName = selectionButton.dataset.selection
-      const selection = SELECTIONS.find(selection => selection.name === selectionName)
-      makeSelection(selection)
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.tabTarget)
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('active')
+    })
+    tabs.forEach(tab => {
+      tab.classList.remove('active')
+    })
+    tab.classList.add('active')
+    target.classList.add('active')
   })
 })
-
-
-
-function makeSelection(selection) { 
-    const computerSelection = randomSelection()
-    const yourWinner = isWinner(selection, computerSelection);
-    const computerWinner = isWinner(selection, yourWinner)
-    console.log(computerSelection)
-
-    addSelectionResult(computerSelection, computerWinner)
-    addSelectionResult(selection, yourWinner)
-
-    if (yourWinner)incrementScore(yourScoreSpan)
-    if (computerWinner)incrementScore(computerScoreSpan)
-    
-}
-function incrementScore(scoreSpan) {
-    scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1
-}
-
-
-
-
-
-function addSelectionResult(selection, winner) {
-    const div = document.createElement('div')
-    div.innerText = (selection.emoji)
-    div.classList.add('result-selection')
-    if (winner) div.classList.add('winner') 
-    finalColumn.after(div)   
-}
-    
-
-
-function isWinner(selection, opponentSelection) {
-    return selection.beats === opponentSelection.name;
-}
-
-function randomSelection() {
-    const randomIndex = Math.floor(Math.random() * SELECTIONS.length)
-    return SELECTIONS[randomIndex]
-}
